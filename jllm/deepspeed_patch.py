60484b318fbade1878d73ae89f2463f8518b2508
janelu9 <652031063@qq.com>
2025-08-11 20:14:51 +0800

    fix important bug

--- jllm/deepspeed_patch.py
+++ jllm/deepspeed_patch.py
@@ -145,6 +145,103 @@ if args.rlhf:
             self.update_hp_grads(clear_lp_grads=clear_lp_grads)
             
     bf16_optimizer.BF16_Optimizer.backward = backward
+    
+    from deepspeed.runtime.pipe.engine import (
+        PartitionedTensor,
+        BACKWARD_MICRO_TIMER,
+        BACKWARD_GLOBAL_TIMER,
+        BACKWARD_INNER_MICRO_TIMER,
+        BACKWARD_INNER_GLOBAL_TIMER
+    )
+    
+    counter_0 = -1
+    
+    def _exec_backward_pass(self, buffer_id):
+        
+        global counter_0
+        counter_0 = (counter_0 + 1) % group_step
+        
+        assert self.optimizer is not None, "must provide optimizer during " \
+                                           "init in order to use backward"
+
+        self.mem_status('BEFORE BWD', reset_max=True)
+
+        # The last stage just runs backward on the loss using DeepSpeed's typical
+        # mechanisms.
+        if self.is_last_stage():
+            super().backward(self.loss)
+            self.mem_status('AFTER BWD')
+            return
+
+        outputs = self.pipe_buffers['outputs'][buffer_id]
+
+        if self.wall_clock_breakdown():
+            self.timers(BACKWARD_MICRO_TIMER).start()
+            self.timers(BACKWARD_GLOBAL_TIMER).start()
+            self.timers(BACKWARD_INNER_MICRO_TIMER).start()
+            self.timers(BACKWARD_INNER_GLOBAL_TIMER).start()
+
+        # Reconstruct if we previously partitioned the output. We must be
+        # careful to also restore the computational graph of the tensors we partitioned.
+        if self.is_pipe_partitioned:
+            if self.is_grad_partitioned:
+                if self.pipe_partition_output_meta_cache is None:
+                    self.pipe_partition_output_meta_cache = outputs[0].to('cpu')
+                part_output = PartitionedTensor.from_meta(meta=self.pipe_partition_output_meta_cache,
+                                                          local_part=outputs[1],
+                                                          group=self.grid.get_slice_parallel_group())
+                self.pipe_buffers['output_tensors'][buffer_id].data = part_output.full()
+                outputs = (self.pipe_buffers['output_tensors'][buffer_id], *outputs[2:])
+            else:
+                # Already restored from partition
+                self.pipe_buffers['output_tensors'][buffer_id].data = outputs[0]
+                outputs = (self.pipe_buffers['output_tensors'][buffer_id], *outputs[1:])
+
+        grad_tensors = self.grad_layer
+        if self.is_grad_partitioned:
+            #print(f'RANK={self.global_rank} BEFORE-BWD restoring grad={self.grad_layer[0].size()} {self.grad_layer[1].size()}')
+            if self.grad_partition_grad_layer_meta_cache is None:
+                self.grad_partition_grad_layer_meta_cache = self.grad_layer[0].to('cpu')
+            part_grad = PartitionedTensor.from_meta(meta=self.grad_partition_grad_layer_meta_cache,
+                                                    local_part=self.grad_layer[1],
+                                                    group=self.grid.get_slice_parallel_group())
+            grad_tensors = (part_grad.full(), *grad_tensors[2:])
+            part_grad = None
+            #print(f'RANK={self.global_rank} BEFORE-BWD restored grad={self.grad_layer[0].size()} {self.grad_layer[1].size()}')
+
+        if self.using_bf16_optimizer and not self.is_last_stage():
+            # manually call because we don't call optimizer.backward()
+            self.optimizer.clear_lp_grads()
+
+        # This handles either a single tensor or tuple of tensors.
+        if isinstance(outputs, tuple):
+            out_tensors = [t for t in outputs if t.is_floating_point()]
+            assert len(out_tensors) == len(grad_tensors)
+            torch.autograd.backward(tensors=out_tensors,retain_graph=counter_0+1<group_step, grad_tensors=grad_tensors)
+        else:
+            torch.autograd.backward(tensors=(outputs, ),retain_graph=counter_0+1<group_step, grad_tensors=(grad_tensors, ))
+
+        if self.using_bf16_optimizer and not self.is_last_stage():
+            # manually call because we don't call optimizer.backward()
+            if not self._config.bfloat16_immediate_grad_update:
+                self.optimizer.update_hp_grads(clear_lp_grads=False)
+
+        # Free up the memory from the output of forward()
+        self.pipe_buffers['output_tensors'][buffer_id] = None
+        self.pipe_buffers['outputs'][buffer_id] = None
+        grad_tensors = None
+
+        if self.wall_clock_breakdown():
+            self.timers(BACKWARD_INNER_MICRO_TIMER).stop()
+            self.timers(BACKWARD_INNER_GLOBAL_TIMER).stop()
+            self.timers(BACKWARD_MICRO_TIMER).stop()
+            self.timers(BACKWARD_GLOBAL_TIMER).stop()
+
+        self.mem_status('AFTER BWD')
+        
+    from deepspeed.runtime.pipe import engine
+    engine._exec_backward_pass = _exec_backward_pass
+
 
 if args.expert_parallel_size>1:
     from deepspeed.moe import layer
